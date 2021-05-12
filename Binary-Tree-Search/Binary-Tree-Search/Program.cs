e38323a75745b6dc5e9608e905460c843d03e26e
“Aniket_D” <aniketdakhare@gmail.com>
2021-05-12 17:48:53 +0530

    [Nilima]Added Count of node

---
+++ Binary-Tree-Search/Binary-Tree-Search/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Welcome to Binary Search Tree********");
+
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertDataInBST(23);
+            binarySearchTree.InsertDataInBST(344);
+            binarySearchTree.InsertDataInBST(43);
+            binarySearchTree.InsertDataInBST(266);
+            binarySearchTree.InsertDataInBST(232);
+            binarySearchTree.DisplayDatainBST();
+            binarySearchTree.CountOfNodes();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Binary-Tree-Search/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Binary_Tree_Search;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
