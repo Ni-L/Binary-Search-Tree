41979ac6d6946b1daa1b7890a49e35e75a1591be
“Aniket_D” <aniketdakhare@gmail.com>
2021-05-12 18:34:03 +0530

    [Nilima]Added Search for element

--- Binary-Tree-Search/Binary-Tree-Search/Program.cs
+++ Binary-Tree-Search/Binary-Tree-Search/Program.cs
@@ -17,9 +17,18 @@ namespace Binary_Tree_Search
             binarySearchTree.InsertDataInBST(344);
             binarySearchTree.InsertDataInBST(43);
             binarySearchTree.InsertDataInBST(266);
-            binarySearchTree.InsertDataInBST(232);
             binarySearchTree.DisplayDatainBST();
             binarySearchTree.CountOfNodes();
+            //returning node of root element after last element is inserted in the node
+
+            Node<int> head = binarySearchTree.InsertDataInBST(232);
+            //displaying values of node is sorted order
+            binarySearchTree.DisplayDatainBST();
+            //count left and right nodes in binary search tree
+            binarySearchTree.CountOfNodes();
+            //search for the value in binary search tree
+            //root element obtained from the insert data and search value is passed to method 
+            binarySearchTree.SearchInBST(head,232);
             Console.ReadLine();
         }
     }
